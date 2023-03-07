add48f892edfafd59ebe85edbbcc2dc127008ce3
hajime-f <hajime.fujita@gmail.com>
2023-03-07 21:51:46 +0900

    first commit

---
+++ elpa/smartparens-20230219.1728/smartparens-pkg.el
@@ -0,0 +1,13 @@
+(define-package "smartparens" "20230219.1728" "Automatic insertion, wrapping and paredit-like navigation with user defined pairs."
+  '((dash "2.13.0")
+    (cl-lib "0.3"))
+  :commit "f0c863268d296e38d4b5374f4c165cf9a823cd8c" :authors
+  '(("Matus Goljer" . "matus.goljer@gmail.com"))
+  :maintainer
+  '("Matus Goljer" . "matus.goljer@gmail.com")
+  :keywords
+  '("abbrev" "convenience" "editing")
+  :url "https://github.com/Fuco1/smartparens")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ undohist/!Users!hajime-f!Development!hearts!hearts.py
@@ -0,0 +1,8 @@
+
+((digest . "c96de41f9b32cfd9cc9f46ac4fe68803") (undo-list nil (" " . -66) ((marker . 66) . -1) ((marker . 66) . -1) 67 nil (66 . 67) (t 25589 38621 202729 856000) nil (28 . 35) (t 25580 21707 585476 463000) nil (243 . 244) nil ("2" . -243) 244 nil (apply -2 60 128 undo--wrap-and-run-primitive-undo 60 128 ((60 . 62) 104)) (t 25580 21446 509268 185000) nil (574 . 580) (t 25580 21385 991925 599000) nil ("
+" . 546) (t 25580 21383 434186 797000) nil ("    
+    # making agents playing the card game \"Hearts\" in NUM_GAMES games
+    for i in range(st.NUM_GAMES):
+        penalty_points = play_game(agents, i, dir_name)
+        write_penalty_log(penalty_points, dir_name)
+" . 610) 827 nil (610 . 614) nil ("    " . -610) 614 nil ("        " . -614) 610 nil (607 . 609) nil (597 . 607) ("play" . -597) 601 nil (597 . 601) nil ("g" . -597) 598 nil (589 . 598) nil (575 . 589) ("pena" . -575) 579 nil (575 . 579) nil ("p" . -575) ("e" . -576) ("n" . -577) ("o" . -578) 579 nil (576 . 579) nil (575 . 576) nil (570 . 575) (t 25580 20959 452476 309000) nil ("g" . 713) nil (713 . 714) (t 25580 20959 452476 309000) nil ("game." . 713) (t 25580 20947 636410 444000) nil (713 . 718) (t 25580 20928 327697 569000) nil ("h" . 713) nil (713 . 714) (t 25580 20928 327697 569000)))
