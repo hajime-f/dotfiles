af45441cad7caf81c425270a8b0f2320a11cf0ef
Hajime Fujita <hajime.fujita@gmail.com>
2025-02-27 16:06:57 +0900

    init.el を編集した

---
+++ undohist/!Users!hajime-f!.emacs.d!init.el
@@ -0,0 +1,2 @@
+
+((digest . "f47880a4282674a93f915286504a7781") (undo-list nil ("Commentary: " . -5) ((marker . 5) . -7) ((marker . 5) . -7) ((marker . 56) . -12) ((marker) . -12) 17 nil (5 . 17) (t 26559 46295 503608 836000)))
---
+++ undohist/!Users!hajime-f!Development!dotfiles!custom.el
@@ -0,0 +1,12 @@
+
+((digest . "998ad30fb9ebccfc31cd3ded0f79b3c4") (undo-list (333 . 572) ("(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )" . -333) (1 . 332) ("(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(doom-themes blackout el-get hydra leaf-keywords leaf)))" . -1) (t 26559 14234 469961 182000)))
